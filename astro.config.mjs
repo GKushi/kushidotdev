b240905c63e1b867c63e5fa9ff71a16c534f98d2
GKushi <gaworski.kasper@gmail.com>
2024-10-25 00:23:20 +0200

    Add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,15 @@ import sitemap from '@astrojs/sitemap';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/static';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kushi.dev',
   integrations: [tailwind(), sitemap(), react()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
