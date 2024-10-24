a2f3e0a9de816fe085afd06df03911c718563eac
GKushi <gaworski.kasper@gmail.com>
2024-10-24 21:13:35 +0200

    Add header component - Add prettier configuration and lib along with tailwind class sorting

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors: {
-        primary: "#123440",
-        secondary: "#40798C",
-        accent: "#E3DAE7",
-        background: "#ECE6EF",
+        primary: '#123440',
+        secondary: '#40798C',
+        accent: '#E3DAE7',
+        background: '#ECE6EF',
       },
     },
   },
